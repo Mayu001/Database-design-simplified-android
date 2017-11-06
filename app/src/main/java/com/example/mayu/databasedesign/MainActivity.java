b25ad9a48b2cad45c9fdc4a1186e990043b771c8
Mayank Mohit <mayank.mohit1122@gmail.com>
2017-11-06 22:41:20 +0530

    Initial commint (Xml DOne)

---
+++ app/src/main/java/com/example/mayu/databasedesign/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mayu.databasedesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+ Author Mayank Mohit (Mayu)
+ github= https://github.com/Mayu001
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
